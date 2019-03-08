0fd8537b22e318cb1b26c8edd0a05291d337ea09
Alan Charles <alanjcharles14@gmail.com>
2019-03-08 10:01:22 -0700

    Add header to admin

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './admin.css'
+
+const Admin = (props) => {
+  return (
+    <div className='admin-container'>
+      <div className='header-container'>
+        <div className='header-container-2'>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Admin; 
