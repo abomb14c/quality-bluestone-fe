787f4566ecd88ebebba90d98c197520b475f3102
Alan Charles <alanjcharles14@gmail.com>
2019-03-08 10:37:58 -0700

    Add weather api

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,14 +1,9 @@
-var GoogleAuth; // Google Auth object.
-function initClient() {
-  gapi.client.init({
-      'apiKey': 'YOUR_API_KEY',
-      'clientId': 'YOUR_CLIENT_ID',
-      'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
-      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
-  }).then(function () {
-      GoogleAuth = gapi.auth2.getAuthInstance();
+import weatherKey from './../keys';
 
-      // Listen for sign-in state changes.
-      GoogleAuth.isSignedIn.listen(updateSigninStatus);
-  });
+export const fetchWeather = async() => {
+    const url = `i.openweathermap.org/data/2.5/weather?lat=41.9434&lon=-75.5997&APPID=${weatherKey}`;
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    console.log(weatherData)
+    return weatherData;
 }
