c096c01b0d9d234d2d507cc2d5906db6b64c3be8
Alan Charles <alanjcharles14@gmail.com>
2019-03-11 10:32:09 -0600

    Add new user form

--- src/containers/AddEmployeeForm/AddEmployeeForm.js
+++ src/containers/AddEmployeeForm/AddEmployeeForm.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import './add-employee-form.css';
+
 
 class AddEmployeeForm extends Component {
   constructor(props){
@@ -12,7 +14,8 @@ class AddEmployeeForm extends Component {
       state: '',
       zip: '',
       phone: '',
-      email: ''
+      email: '',
+      role: 'Worker'
     }
   }
 
@@ -26,58 +29,63 @@ class AddEmployeeForm extends Component {
 
   render() {
     return (
-      <div>
-        <form>
+      <div className='add-employee-form-container'>
+        <form className='add-employee-form'>
+          <label>Add A New User</label>
+          <div className='add-user-name-container'>
+            <input
+              className="add-user-name"
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              placeholder="First Name"
+              onChange={this.handleChange}
+            />
+            <input
+              className="add-user-name"
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              placeholder="Last Name"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='add-user-address-container'>
+            <input
+              className="add-user-address"
+              type="text"
+              name="address"
+              value={this.state.address}
+              placeholder="Address"
+              onChange={this.handleChange}
+            />
+            <input
+              className="add-user-city"
+              type="text"
+              name="city"
+              value={this.state.city}
+              placeholder="City"
+              onChange={this.handleChange}
+            />
+            <input
+              className="add-user-state"
+              type="text"
+              name="state"
+              value={this.state.state}
+              placeholder="State"
+              onChange={this.handleChange}
+            />
+            <input
+              className="add-user-zip"
+              type="text"
+              name="zip"
+              value={this.state.zip}
+              placeholder="Zip Code"
+              onChange={this.handleChange}
+            />
+          </div>
           <input
-            className="add-user"
-            type="text"
-            name="firstName"
-            value={this.state.firstName}
-            placeholder="First Name"
-            onChange={this.handleChange}
-          />
-          <input
-            className="add-user"
-            type="text"
-            name="lastName"
-            value={this.state.lastName}
-            placeholder="Last Name"
-            onChange={this.handleChange}
-          />
-          <input
-            className="add-user"
-            type="text"
-            name="address"
-            value={this.state.address}
-            placeholder="Address"
-            onChange={this.handleChange}
-          />
-          <input
-            className="add-user"
-            type="text"
-            name="city"
-            value={this.state.city}
-            placeholder="City"
-            onChange={this.handleChange}
-          />
-          <input
-            className="add-user"
-            type="text"
-            name="state"
-            value={this.state.state}
-            placeholder="State"
-            onChange={this.handleChange}
-          />
-          <input
-            className="add-user"
-            type="text"
-            name="zip"
-            value={this.state.zip}
-            placeholder="Zip Code"
-            onChange={this.handleChange}
-          />
-          <input
-            className="add-user"
+            className="add-user-phone"
             type="text"
             name="phone"
             value={this.state.phone}
@@ -85,13 +93,24 @@ class AddEmployeeForm extends Component {
             onChange={this.handleChange}
           />
           <input
-            className="add-user"
+            className="add-user-email"
             type="text"
             name="email"
             value={this.state.email}
             placeholder="Email"
             onChange={this.handleChange}
           />
+          <select className='select-role'>
+            <option selected value={this.state.role}>Worker</option>
+            <option value={this.state.role}>Driver</option>
+            <option value={this.state.role}>Accountant</option>
+            <option value={this.state.role}>Admin</option>
+          </select>
+          <button
+            className='submit-new-employee'
+          >
+          Add Employee
+          </button>
         </form>
       </div>
     )
