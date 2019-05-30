baf8345f33ea1c0443e978bbccc36a4d774ecbde
Jack Laird <jack.laird0@gmail.com>
2019-05-30 12:45:52 -0600

    Change imports for components and containers

--- src/containers/newHire/newHire.js
+++ src/containers/newHire/newHire.js
@@ -1,66 +1,62 @@
 import React, { Component } from 'react';
 import './new-hire.css';
 
-import ComponentHeader from '../../components/componentHeader/ComponentHeader';
-import QuarryApp from '../QuarryApp/QuarryApp';
-import {Footer} from '../../components/Footer/Footer';
+import { ComponentHeader, Footer } from '../../components';
+import { QuarryApp } from '..';
 
 class NewHire extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      type: null
-    }
+      type: null,
+    };
   }
 
-  handleTypeQuarry = (event) => {
-    event.preventDefault()
-    this.setState ({
-      type: 'quarry'
-    })
-    console.log(this.state)
-  }
+  handleTypeQuarry = event => {
+    event.preventDefault();
+    this.setState({
+      type: 'quarry',
+    });
+    console.log(this.state);
+  };
 
-  render(){ 
+  render() {
     return (
       // <div className='app-container'>
-      <div className='new-hire-container'>
-        <div className='header'>
+      <div className="new-hire-container">
+        <div className="header">
           <ComponentHeader />
         </div>
-        <div className='new-hire-title-container'>
-          <h2 className='new-hire-title'>Application For Employment</h2>
-          <h5 className='new-hire-sub-title'>Pre-Employment Questionnaire</h5>
-          <h5 className='new-hire-sub-title, new-hire-sub-color'>Equal Opportunity Employer</h5>
+        <div className="new-hire-title-container">
+          <h2 className="new-hire-title">Application For Employment</h2>
+          <h5 className="new-hire-sub-title">Pre-Employment Questionnaire</h5>
+          <h5 className="new-hire-sub-title, new-hire-sub-color">
+            Equal Opportunity Employer
+          </h5>
         </div>
-        <div className='type-container'>
-          <div className='quarry-container, type-sub-container'>
-            <h5 className='type-title'>Quarry Application</h5>
-            <div 
+        <div className="type-container">
+          <div className="quarry-container, type-sub-container">
+            <h5 className="type-title">Quarry Application</h5>
+            <div
               onClick={this.handleTypeQuarry}
-              className='plus-button'
-              name= 'type'
-              value='quarry'
-            >
-            </div>
+              className="plus-button"
+              name="type"
+              value="quarry"
+            />
           </div>
-          <div className='truck-container, type-sub-container'>
-            <h5 className='type-title'>Truck Driver Application</h5>
-            <div 
-              className='plus-button'
-            >
-            </div>
+          <div className="truck-container, type-sub-container">
+            <h5 className="type-title">Truck Driver Application</h5>
+            <div className="plus-button" />
           </div>
         </div>
-        {this.state.type === 'quarry' &&
-        <div className='quarry-app-section'>
-          <QuarryApp />
-       </div>
-        }
+        {this.state.type === 'quarry' && (
+          <div className="quarry-app-section">
+            <QuarryApp />
+          </div>
+        )}
       </div>
-   
-    )
+    );
   }
 }
 
