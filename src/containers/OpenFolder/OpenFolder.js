acb4b06bf6cb7e0816e2761206c8a900d8092f44
Jack Laird <jack.laird0@gmail.com>
2019-05-30 12:45:46 -0600

    Change imports for components and containers

--- src/containers/OpenFolder/OpenFolder.js
+++ src/containers/OpenFolder/OpenFolder.js
@@ -1,58 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Redirect } from 'react-router-dom';
-import './open-folder.css'; 
-import ComponentHeader from '../../components/componentHeader/ComponentHeader';
-import UploadFiles from '../uploadFiles/uploadFiles';
-import {clearFolder} from '../../actions/updateFolders/updateFolders';
-
+import './open-folder.css';
+import { ComponentHeader } from '../../components';
+import { UploadFiles } from '..';
+import { clearFolder } from '../../actions/updateFolders/updateFolders';
 
 class OpenFolder extends Component {
-  constructor(props){
-    super(props)
-    
-    this.state = {
+  constructor(props) {
+    super(props);
 
-    }
+    this.state = {};
   }
 
   handleBack = () => {
-    this.props.updateFolder()
-  }
+    this.props.updateFolder();
+  };
 
-  render(){
+  render() {
     return (
-      <div className='open-folder-container'>
-      {this.props.folder.active === false && <Redirect to='/admin' />}
+      <div className="open-folder-container">
+        {this.props.folder.active === false && <Redirect to="/admin" />}
         <ComponentHeader />
-        <div className='open-folder-body'>
-        <div className='open-folder-nav'>
-          <div 
-            className='open-folder-back'
-            onClick={this.handleBack}
-          >
-            <div className='open-folder-back-icon'></div>
+        <div className="open-folder-body">
+          <div className="open-folder-nav">
+            <div className="open-folder-back" onClick={this.handleBack}>
+              <div className="open-folder-back-icon" />
+            </div>
+            <div className="open-folder-title-container">
+              <p className="open-folder-title">{this.props.folder.name}</p>
+            </div>
           </div>
-          <div className='open-folder-title-container'>
-            <p className='open-folder-title'>{this.props.folder.name}</p>
-          </div>
-        </div>
-          <div className='upload-files-in-folder'>
+          <div className="upload-files-in-folder">
             <UploadFiles />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export const mapStateToProps = state => ({
-  folder: state.folder
+  folder: state.folder,
 });
 
 export const mapDispatchToProps = dispatch => ({
-  updateFolder: () => dispatch(clearFolder())
-
+  updateFolder: () => dispatch(clearFolder()),
 });
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OpenFolder)); 
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(OpenFolder)
+);
