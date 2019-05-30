7ca0a0dafdf3837777cac26f51cd46a590f320b5
Jack Laird <jack.laird0@gmail.com>
2019-05-30 12:45:41 -0600

    Change imports for components and containers

--- src/containers/EmployeeFolders/EmployeeFolders.js
+++ src/containers/EmployeeFolders/EmployeeFolders.js
@@ -1,63 +1,61 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './employee-folder.css';
 import { Link, Redirect, Switch, withRouter } from 'react-router-dom';
-import AddEmployeeForm from '../AddEmployeeForm/AddEmployeeForm';
-import {connect} from 'react-redux';
-import {closeEmployees} from '../../actions/updateAdmin/updateAdmin';
+import { AddEmployeeForm } from '..';
+import { connect } from 'react-redux';
+import { closeEmployees } from '../../actions/updateAdmin/updateAdmin';
 
 class EmployeeFolders extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      active: false
-    }
+      active: false,
+    };
   }
 
   handleClick = () => {
     this.setState({
-      active: !this.state.active
-    })
-  }
+      active: !this.state.active,
+    });
+  };
 
-  handleBack = (event) => {
+  handleBack = event => {
     event.preventDefault();
-    this.props.handleClose()
-  }
+    this.props.handleClose();
+  };
 
   render() {
-    return(
-      <div className='employee-container'>
-        <div className='back-nav'>
-      </div>
-      {this.state.active === false &&
-        <div className='add-employee'>
-          <h5 className='add-employee-title'>Add Employee</h5>
-          <div 
-            className='employee-button'
-            onClick= {this.handleClick}
-          >
+    return (
+      <div className="employee-container">
+        <div className="back-nav" />
+        {this.state.active === false && (
+          <div className="add-employee">
+            <h5 className="add-employee-title">Add Employee</h5>
+            <div className="employee-button" onClick={this.handleClick} />
           </div>
-        </div>}
-        {this.state.active === true &&
-          <div className='add-employee-form-container-1'>
+        )}
+        {this.state.active === true && (
+          <div className="add-employee-form-container-1">
             <button
-              className='employee-form-cancel-button'
+              className="employee-form-cancel-button"
               onClick={this.handleClick}
             >
-            X
+              X
             </button>
             <AddEmployeeForm />
           </div>
-        }
+        )}
       </div>
-    )
+    );
   }
 }
 
 export const mapDispatchToProps = dispatch => ({
-  handleClose: () => dispatch(closeEmployees())
+  handleClose: () => dispatch(closeEmployees()),
 });
 
-
-export default connect(null, mapDispatchToProps)(EmployeeFolders);
+export default connect(
+  null,
+  mapDispatchToProps
+)(EmployeeFolders);
