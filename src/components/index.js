9707f4982657e925c6b6d88fbab7f3e2de0e4515
Jack Laird <jack.laird0@gmail.com>
2019-06-12 11:44:45 -0600

    Add LeftDrawer tocomponents export file

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import ComponentHeader from './componentHeader/ComponentHeader';
 import FolderWidget from './FolderWidget/FolderWidget';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import LeftDrawer from './LeftDrawer/LeftDrawer';
 import SignInModal from './SignInModal/SignInModal';
 
 export {
@@ -15,5 +16,6 @@ export {
   FolderWidget,
   Footer,
   Header,
+  LeftDrawer,
   SignInModal,
 };
