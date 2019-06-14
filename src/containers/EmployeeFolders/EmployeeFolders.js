a6e89373195c335d9dbf085668cb46ca820f4c68
Jack Laird <jack.laird0@gmail.com>
2019-06-14 13:08:20 -0600

    Render employee widget in folder container

--- src/containers/EmployeeFolders/EmployeeFolders.js
+++ src/containers/EmployeeFolders/EmployeeFolders.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import './employee-folder.css';
 import { Link, Redirect, Switch, withRouter } from 'react-router-dom';
-import { AddEmployeeForm } from '..';
+import { AddEmployeeForm, EmployeeWidget } from '..';
 import { connect } from 'react-redux';
 import { closeEmployees } from '../../actions';
+import { fetchAllUsers } from '../../apiCalls/apiCalls';
+import { retrieveSessionStorage } from '../../util/component-helpers/componentHelpers';
+import { compose } from 'recompose';
+import { Typography, withStyles } from '@material-ui/core';
+
+const styles = theme => ({
+  root: {
+    height: '100%',
+  },
+  label: {
+    margin: `0 0 ${theme.spacing(3)}px 0`,
+  },
+});
 
 class EmployeeFolders extends Component {
   constructor(props) {
@@ -11,8 +23,25 @@ class EmployeeFolders extends Component {
 
     this.state = {
       active: false,
+      employees: [],
     };
   }
+
+  async componentDidMount() {
+    await retrieveSessionStorage('employees', this.setState.bind(this));
+
+    if (!this.state.employees.length) {
+      const employees = await fetchAllUsers();
+      this.setState({ employees });
+      sessionStorage.setItem('employees', JSON.stringify(employees));
+    }
+  }
+
+  // displayEmployees = () => {
+  //   return this.state.employees.map((employee, ind) => {
+  //     return <EmployeeWidget employee={employee} key={`employee-${ind}`} />;
+  //   });
+  // };
 
   handleClick = () => {
     this.setState({
@@ -26,9 +55,14 @@ class EmployeeFolders extends Component {
   };
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="employee-container">
-        <div className="back-nav" />
+      <div className={classes.root}>
+        <Typography variant="h6" className={classes.label}>
+          employees
+        </Typography>
+        {/* <div className="back-nav" />
         {this.state.active === false && (
           <div className="add-employee">
             <h5 className="add-employee-title">Add Employee</h5>
@@ -45,7 +79,8 @@ class EmployeeFolders extends Component {
             </button>
             <AddEmployeeForm />
           </div>
-        )}
+        )} */}
+        <EmployeeWidget employees={this.state.employees} />
       </div>
     );
   }
@@ -55,7 +90,10 @@ export const mapDispatchToProps = dispatch => ({
   handleClose: () => dispatch(closeEmployees()),
 });
 
-export default connect(
-  null,
-  mapDispatchToProps
+export default compose(
+  connect(
+    null,
+    mapDispatchToProps
+  ),
+  withStyles(styles)
 )(EmployeeFolders);
